6c8f0ee9265ff66f2f8a5f57193dabd1728b49b8
Uzair Hussain <uzairhussain98@hotmail.com>
2022-11-10 01:21:47 +0500

    developed UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import InputField from './components/InputField';
 
 function App() {
+
+  const [todo, setTodo] = useState('')
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 className='heading'>Todo App TS</h1>
+      <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
