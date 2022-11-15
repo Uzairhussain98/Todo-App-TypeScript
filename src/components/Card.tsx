7fd638dff06e75e39333d15c021159ecda3c294a
Uzair Hussain <uzairhussain98@hotmail.com>
2022-11-15 16:45:27 +0500

    completed

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Todo } from "../model";
 import "./styles.css";
 import { AiFillEdit, AiFillDelete } from "react-icons/ai";
@@ -12,6 +12,14 @@ interface Props {
 }
 
 const Card = ({ todo, todos, setTodos }: Props) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>(todo.task);
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [edit]);
+
   const handleDelete = (id: number) => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
@@ -23,17 +31,37 @@ const Card = ({ todo, todos, setTodos }: Props) => {
       )
     );
   };
-  // setTodos(todos.map((todo) => todo.id === id ? { ...todo, isDone: !todo.isDone } : todo));
+
+  const handleEdit = () => {
+    if (!edit && !todo.isDone) {
+      setEdit(!edit);
+    }
+  };
+
+  const handleEditUpdate = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, task: editTodo } : todo))
+    );
+    setEdit(false);
+  };
 
   return (
-    <div className="card">
-      {todo.isDone ? (
+    <form className="card" onSubmit={(e) => handleEditUpdate(e, todo.id)}>
+      {edit ? (
+        <input
+          ref={inputRef}
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+        />
+      ) : todo.isDone ? (
         <s className="card_text">{todo.task}</s>
       ) : (
         <span className="card_text">{todo.task}</span>
       )}
+
       <div>
-        <span className="icon">
+        <span className="icon" onClick={() => handleEdit()}>
           <AiFillEdit />
         </span>
         <span className="icon" onClick={() => handleDelete(todo.id)}>
@@ -43,7 +71,7 @@ const Card = ({ todo, todos, setTodos }: Props) => {
           <MdDone />
         </span>
       </div>
-    </div>
+    </form>
   );
 };
 
